a553163ceadeef360f3af747ebe11d9aeb38de83
sid-4998 <siddhantjha34@gmail.com>
2025-12-15 10:45:53 +0530

    Setup react application using Vite build tool

---
+++ react-Introduction/src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<h1>Hello, World!</h1>);
+
+// Step 1: npm create vite@latest
+// Step 2: Name your project
+// Step 3: Name your package.json file
+// Step 4: Choose a framework 
+// Step 5: Choose the preferred language of framework
